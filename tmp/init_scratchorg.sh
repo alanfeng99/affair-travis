0939c1bbe819c3db2afd2414e5d11cb64a14b270
jinyen0531 <jinyen0531@gmail.com>
2018-07-30 14:43:03 +0800

    admin

--- tmp/init_scratchorg.sh
+++ tmp/init_scratchorg.sh
@@ -1,2 +1,9 @@
-#!bin/sh
-echo tesg
+$alias=`date '+%Y%m%d%H%M%S'`
+sfdx force:org:create -f ../config/project-scratch-def.json --setalias $alias --setdefaultusername --wait 2
+sfdx force:source:push
+sfdx force:user:permset:assign --permsetname Scratch
+sfdx force:data:tree:import -f sfdx-out/export-demo-SubscriptionProgram__cs.json
+sfdx force:data:tree:import -f sfdx-out/export-demo-Product2s.json
+sfdx force:data:tree:import -f sfdx-out/export-demo-Address__cs.json
+sfdx force:data:tree:import -f sfdx-out/export-demo-Address__cs2.json
+sfdx force:data:tree:import -f sfdx-out/export-demo-EncryptionKey__cs.json
