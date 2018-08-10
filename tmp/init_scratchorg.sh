fde83d3d7a8b768e6bd2ffb802637a729566e9bf
jinyen0531 <jinyen0531@gmail.com>
2018-08-10 09:57:07 +0800

    delete some unless

--- tmp/init_scratchorg.sh
+++ tmp/init_scratchorg.sh
@@ -1,4 +1,4 @@
-$alias=`date '+%Y%m%d%H%M%S'`
+$alias="date '+%Y%m%d%H%M%S'"
 sfdx force:org:create -f ../config/project-scratch-def.json --setalias $alias --setdefaultusername --wait 2
 sfdx force:source:push
 sfdx force:user:permset:assign --permsetname Scratch
